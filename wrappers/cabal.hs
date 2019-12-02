de954c4681c4c99b9418ea7b8a76fa796604ed78
fendor <power.walross@gmail.com>
2019-12-02 17:33:50 +0100

    Add environment variables for ghc.
    * On windows, use either ghc or stack to compile the wrapper.
    * Parse .ghci script to obtain all targets.
    * Change tests to fit the new target selection.

--- wrappers/cabal.hs
+++ wrappers/cabal.hs
@@ -9,6 +9,8 @@ import System.IO (openFile, hClose, hPutStrLn, IOMode(..))
 main = do
   args <- getArgs
   output_file <- getEnv "HIE_BIOS_OUTPUT"
+  ghcPath <- getEnv "HIE_BIOS_GHC"
+  ghcPathArgs <- fmap unwords (getEnv "HIE_BIOS_GHC_ARGS")
   case args of
     "--interactive":_ -> do
       h <- openFile output_file AppendMode
@@ -16,6 +18,6 @@ main = do
       mapM_ (hPutStrLn h) args
       hClose h
     _ -> do
-      ph <- spawnProcess "ghc" args
+      ph <- spawnProcess ghcPath (ghcPathArgs ++ args)
       code <- waitForProcess ph
       exitWith code
