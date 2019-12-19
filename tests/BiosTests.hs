8aa847b4b408e301addd34d276a4afeb8bcea00f
fendor <power.walross@gmail.com>
2019-12-19 12:30:34 +0100

    Fix test-cases for windows

--- tests/BiosTests.hs
+++ tests/BiosTests.hs
@@ -10,6 +10,7 @@ import Control.Monad.IO.Class
 import Control.Monad ( unless, forM_ )
 import System.Directory
 import System.FilePath ( makeRelative, (</>) )
+import System.Info.Extra ( isWindows )
 
 main :: IO ()
 main = do
@@ -31,21 +32,25 @@ main = do
                   (Just "./tests/projects/simple-cabal/hie.yaml")
                 )
         ]
-      , testGroup "Loading tests" [
-        testCaseSteps "simple-cabal" $ testDirectory "./tests/projects/simple-cabal/B.hs"
-        , testCaseSteps "simple-stack" $ testDirectory "./tests/projects/simple-stack/B.hs"
-        , testCaseSteps "simple-direct" $ testDirectory "./tests/projects/simple-direct/B.hs"
-        , testCaseSteps "simple-bios" $ testDirectory "./tests/projects/simple-bios/B.hs"
-        , testCaseSteps "multi-cabal" {- tests if both components can be loaded -}
-                      $  testDirectory "./tests/projects/multi-cabal/app/Main.hs"
-                      >> testDirectory "./tests/projects/multi-cabal/src/Lib.hs"
-        , testCaseSteps "multi-stack" {- tests if both components can be loaded -}
-                      $  testDirectory "./tests/projects/multi-stack/app/Main.hs"
-                      >> testDirectory "./tests/projects/multi-stack/src/Lib.hs"
-        ]
+      , testGroup "Loading tests"
+        $ linuxExlusiveTestCases
+        ++
+           [ testCaseSteps "simple-cabal" $ testDirectory "./tests/projects/simple-cabal/B.hs"
+           , testCaseSteps "simple-stack" $ testDirectory "./tests/projects/simple-stack/B.hs"
+           , testCaseSteps "simple-direct" $ testDirectory "./tests/projects/simple-direct/B.hs"
+           , testCaseSteps "multi-cabal" {- tests if both components can be loaded -}
+                         $  testDirectory "./tests/projects/multi-cabal/app/Main.hs"
+                         >> testDirectory "./tests/projects/multi-cabal/src/Lib.hs"
+           , testCaseSteps "multi-stack" {- tests if both components can be loaded -}
+                         $  testDirectory "./tests/projects/multi-stack/app/Main.hs"
+                         >> testDirectory "./tests/projects/multi-stack/src/Lib.hs"
+           ]
       ]
 
-
+linuxExlusiveTestCases :: [TestTree]
+linuxExlusiveTestCases
+  | isWindows = []
+  | otherwise = [ testCaseSteps "simple-bios" $ testDirectory "./tests/projects/simple-bios/B.hs" ]
 
 testDirectory :: FilePath -> (String -> IO ()) -> IO ()
 testDirectory fp step = do
