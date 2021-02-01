cda8888b07d1484f61ca4a45779b4dd6996681ef
Hécate Moonlight <hecate+github@glitchbra.in>
2021-02-01 20:17:59 +0100

    Use the proper GHC version given by cabal
    Proof against NoImplicitPrelude
    
    fix imports
    
    Add ghc-pkg wrapper
    
    Add documentation to new functions
    
    Construct ghc-pkg name from ghc filename
    
    Replace 'ghc' with 'ghc-pkg' in the filename of the ghc executable. This
    is better than assuming the executable starts with a 'ghc-' prefix, as
    some distros provide a "triple" for the executable, e.g. 'x86_64-redhat-linux-ghc'.
    
    Replacing 'ghc' with 'ghc-pkg' works for these distros as well and,
    additionally, we don't have to manually take care of the '.exe' suffix.
    
    Clear package-env for ghc executable path discovery
    
    Refactor 'cabalProcess' function
    
    Undo indentation changes and improve naming
    
    Remove verbose mode from wrapper

--- wrappers/cabal.hs
+++ wrappers/cabal.hs
@@ -1,7 +1,8 @@
 module Main (main) where
 
+import Data.Maybe (fromMaybe)
 import System.Directory (getCurrentDirectory)
-import System.Environment (getArgs, getEnv)
+import System.Environment (getArgs, getEnv, lookupEnv)
 import System.Exit (exitWith)
 import System.Process (spawnProcess, waitForProcess)
 import System.IO (openFile, hClose, hPutStrLn, IOMode(..))
@@ -17,6 +18,7 @@ main = do
       mapM_ (hPutStrLn h) args
       hClose h
     _ -> do
-      ph <- spawnProcess "ghc" (args)
+      ghc_path <- fromMaybe "ghc" <$> lookupEnv "HIE_BIOS_GHC"
+      ph <- spawnProcess ghc_path (args)
       code <- waitForProcess ph
       exitWith code
