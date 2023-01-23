2f46a218a1522b5b627923933e7cd777b2a0d2f3
Zubin Duggal <zubin.duggal@gmail.com>
2023-01-23 13:25:11 +0530

    compat

--- src/HIE/Bios/Wrappers.hs
+++ src/HIE/Bios/Wrappers.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE CPP #-}
 module HIE.Bios.Wrappers (cabalWrapper, cabalWrapperHs) where
 
-import Data.FileEmbed hiding (makeRelativeToProject)
+import Data.FileEmbed
+#if __GLASGOW_HASKELL__ >= 903
+  hiding (makeRelativeToProject)
+#endif
 import Language.Haskell.TH.Syntax
 
 cabalWrapper :: String
